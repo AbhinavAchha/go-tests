9f57e85458f57ba59c128b56f64d2334c6acabe0
Abhinav Achha <achhaabhinav@gmail.com>
2023-07-27 15:30:03 +0530

    add bytes-string tests

---
+++ bytes-string/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+var s string
+
+var by []byte
+
+var ss = "ex"
+
+var byy = []byte("ex")
+
+func BenchmarkBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bytes.Split(by, byy)
+	}
+}
+
+func BenchmarkString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.Split(s, ss)
+	}
+}
+
+func init() {
+	s = strings.Repeat(
+		"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+		10,
+	)
+
+	by = []byte(s)
+}
