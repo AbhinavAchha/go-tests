1415348fad866ae4864cc220d7678f91b3fcb2be
Abhinav Achha <achhaabhinav@gmail.com>
2023-08-20 12:33:14 +0530

    add fmt.Sprintf vs fmt.Fprintf test

---
+++ fmt-sprintf-vs-fprintf/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+var longString = strings.Repeat("a", 1000) + "%s" + strings.Repeat("b", 1000)
+
+func BenchmarkSprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b := new(strings.Builder)
+		b.WriteString(fmt.Sprintf(longString, "c"))
+	}
+}
+
+func BenchmarkFprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b := new(strings.Builder)
+		fmt.Fprintf(b, longString, "c")
+	}
+}
+
+/*
+goos: linux
+goarch: amd64
+pkg: ok
+cpu: AMD Ryzen 5 4500U with Radeon Graphics
+BenchmarkSprintf-6        344295              3786 ns/op            4097 B/op          2 allocs/op
+BenchmarkFprintf-6        344166              3228 ns/op            2080 B/op          2 allocs/op
+PASS
+ok      ok      2.494s
+*/
